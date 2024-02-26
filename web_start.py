01040e40fe136edca0ca41e749b3473b878695b5
norlandsoft <11669556@qq.com>
2024-02-26 17:24:13 +0800

    call zhipuai by chain

--- web_start.py
+++ web_start.py
@@ -1,48 +1,46 @@
 # 启动FastAPI服务，提供聊天机器人服务
-import os
-from dotenv import load_dotenv
-
 import uvicorn
-import asyncio
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-from zhipuai import ZhipuAI
 from starlette.responses import StreamingResponse
 
 from server import ChatService
+from server.llm.ChatZhipuAI import ChatZhipuAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+llm = ChatZhipuAI(
+    temperature=0.1,
+    api_key="06cd4940767ac97f37f42340ededc7cb.NByo90Rv4FAieZLD",
+    model_name="glm-4",
+    streaming=True
+)
+
+# 提问模版
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "你是世界级的技术文档作者。"),
+    ("user", "{input}")
+])
 
 app = FastAPI()
-
-# 从.env中读取环境变量
-load_dotenv()
-zhipu_api_key = os.getenv('qianwen_api_key')
-
-client = ZhipuAI(api_key=zhipu_api_key)
 
 class ChatMessage(BaseModel):
     content: str = None
 
 async def send_message(content: str):
-    response = client.chat.completions.create(
-        model="GLM-4",  # 需要调用的模型名称
-        messages=[
-            {"role": "system", "content": "你是一个乐于解答各种问题的助手，你的任务是为用户提供专业、准确、有见地的建议。"},
-            {"role": "user", "content": content},
-        ],
-        stream=True,
-        max_tokens=4096,
-    )
+    output_parser = StrOutputParser()
+    chain = prompt | llm | output_parser
 
-    for chunk in response:
-        print(chunk.choices[0].delta.content)
-        yield f"{chunk.choices[0].delta.content}"
-        await asyncio.sleep(0)
+    for chunk in chain.stream({"input": content}):
+        yield f"{chunk}"
+        # await asyncio.sleep(0)
 
 @app.post("/v1/chat/completions")
 async def completions(chat: ChatMessage):
     return StreamingResponse(send_message(chat.content), media_type="text/event-stream")
 
 if __name__ == "__main__":
+    # 初始化数据库连接
     ChatService.init_db()
     uvicorn.run(app, host="0.0.0.0", port=8080)
